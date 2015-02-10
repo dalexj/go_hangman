46a21ba6dcd66875b073e8198aa7eb2a45287532
Alex <96dalex@gmail.com>
2015-02-09 17:50:11 -0700

    input acceptance working

--- hangman.go
+++ hangman.go
@@ -2,17 +2,34 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
+func isLetter(letter string) bool {
+	return letter[0] >= 65 && letter[0] <= 90
+}
+
+func getInput(input *string) {
+	fmt.Print("> ")
+	fmt.Scanln(input)
+	*input = strings.ToUpper(strings.TrimSpace(*input))
+	if len(*input) != 1 || !isLetter(*input) {
+		fmt.Println("Invalid input, please enter a single letter")
+		getInput(input)
+	}
+}
+
+func welcomeMessage() {
+	fmt.Println("Welcome to hangman, here's your phrase to guess:")
+}
+
 func main() {
-	// var input string
+	var input string
 	hangman := NewGame("hello world")
 	hangman.GuessedLetters = "HEO"
-	fmt.Println("Welcome to hangman, here's your phrase to guess:")
+	welcomeMessage()
 	fmt.Println(hangman.FormatPhrase())
-	fmt.Println("H E L L O   W O R L D")
-	// fmt.Print("> ")
-	// fmt.Scanln(&input)
+	getInput(&input)
 	// fmt.Println("H _ _ _ _   _ _ _ _ _")
 	// fmt.Println("H _ _ _ _   _ _ _ _ _")
 	// fmt.Println("H E _ _ _   _ _ _ _ _")
